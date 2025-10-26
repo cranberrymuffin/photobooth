2bb9227b3f219e69dd8eab2b8f50f5246e53d22c
bradley <11279144+redSlug@users.noreply.github.com>
2025-10-26 16:43:42 -0400

    added photo taking indicator, refactored photo taking code, added prettier

--- src/Photobooth.tsx
+++ src/Photobooth.tsx
@@ -1,12 +1,134 @@
 import "./Photobooth.css";
-import CustomWebcam from "./CustomWebcam";
+import Webcam from "react-webcam";
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 const Photobooth = () => {
-  return (
-    <div className="Photobooth">
-      <CustomWebcam />
-    </div>
-  );
+  const webcamRef = useRef<Webcam>(null);
+  const [imgSrc1, setImgSrc1] = useState<string | undefined>(undefined);
+  const [imgSrc2, setImgSrc2] = useState<string | undefined>(undefined);
+  const [imgSrc3, setImgSrc3] = useState<string | undefined>(undefined);
+  const [imgSrc4, setImgSrc4] = useState<string | undefined>(undefined);
+  const [photoStrip, setPhotoStrip] = useState<string | undefined>(undefined);
+  const [isTakingPhotos, setIsTakingPhotos] = useState<boolean>(false);
+  const [isTakingPhoto, setIsTakingPhoto] = useState<boolean>(false);
+
+  useEffect(() => {
+    console.log({ imgSrc1, imgSrc2, imgSrc3, imgSrc4 });
+
+    if (imgSrc1 && imgSrc2 && imgSrc3 && imgSrc4) {
+      const img1 = new Image();
+      const img2 = new Image();
+      const img3 = new Image();
+      const img4 = new Image();
+
+      img1.src = imgSrc1;
+      img2.src = imgSrc2;
+      img3.src = imgSrc3!;
+      img4.src = imgSrc4!;
+
+      img1.onload = () => {
+        img2.onload = () => {
+          img3.onload = () => {
+            img4.onload = () => {
+              const canvas = document.createElement("canvas");
+              const ctx = canvas.getContext("2d");
+
+              const border = 5;
+              canvas.width = img1.width + border * 2;
+              canvas.height =
+                img1.height +
+                img2.height +
+                img3.height +
+                img4.height +
+                border * 5;
+
+              ctx!.fillStyle = "pink";
+              ctx!.fillRect(0, 0, canvas.width, canvas.height);
+
+              ctx!.drawImage(img1, border, border);
+              ctx!.drawImage(img2, border, border * 2 + img1.height);
+              ctx!.drawImage(
+                img3,
+                border,
+                border * 3 + img1.height + img2.height,
+              );
+              ctx!.drawImage(
+                img4,
+                border,
+                border * 4 + img1.height + img2.height + img3.height,
+              );
+
+              setPhotoStrip(canvas.toDataURL("image/png"));
+            };
+          };
+        };
+      };
+    }
+  }, [imgSrc1, imgSrc2, imgSrc3, imgSrc4]);
+
+  function capturePhoto(
+    photoNumber: number,
+    setImgSrc: Dispatch<SetStateAction<string | undefined>>,
+  ) {
+    setTimeout(
+      () => {
+        setIsTakingPhoto(true);
+      },
+      startDelay * photoNumber + photoTakingTime * (photoNumber - 1),
+    );
+    setTimeout(
+      () => {
+        setImgSrc(webcamRef.current?.getScreenshot()!);
+        setIsTakingPhoto(false);
+      },
+      (startDelay + photoTakingTime) * photoNumber,
+    );
+  }
+
+  // create a capture function
+  const startDelay = 3000;
+  const photoTakingTime = 1000;
+
+  const capture = useCallback(() => {
+    setIsTakingPhotos(true);
+    capturePhoto(1, setImgSrc1);
+    capturePhoto(2, setImgSrc2);
+    capturePhoto(3, setImgSrc3);
+    capturePhoto(4, setImgSrc4);
+  }, []);
+
+  if (
+    imgSrc1 === undefined ||
+    imgSrc2 === undefined ||
+    imgSrc3 === undefined ||
+    imgSrc4 === undefined
+  ) {
+    return (
+      <div
+        className={
+          isTakingPhoto
+            ? "container photo-booth taking-photo"
+            : "container photo-booth"
+        }
+      >
+        <Webcam height={600} width={600} ref={webcamRef} />
+        <div className="btn-container">
+          <button onClick={capture} hidden={isTakingPhotos}>
+            Capture photo
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return <img src={photoStrip!} alt="webcam" />;
 };
 
 export default Photobooth;
--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,12 @@ import Photobooth from "./Photobooth";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <Photobooth />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
